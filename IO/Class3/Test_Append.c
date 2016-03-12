0d3f30135fa040571fca12730b673188f976ac09
Kaiser.Xiao <KaiserFromXiao@gmail.com>
2016-03-12 15:58:00 +0800

    Update Test_Append.c

--- IO/Class3/Test_Append.c
+++ IO/Class3/Test_Append.c
@@ -4,7 +4,7 @@
 int main(int argc,char *argv[] )
 {
 	int fd;
-	fd=open("./a.c",O_APPEND|O_RDWR);
+	fd=open("./a.c",O_APPEND|O_RDWR);		//O_RDWR是可写可读，O_APPEND是追加
 	if(fd<0)
 	{
 		printf("open file a.c failure\n");
