f1c7d13dd3fc48ce4aa01734b915cd131b61a5bb
KaiserXiao <KaiserFromXiao@gmail.com>
2016-03-13 15:33:27 +0800

    Add Class7

--- IO/Class7_fgets/Test_fgets.c
+++ IO/Class7_fgets/Test_fgets.c
@@ -6,13 +6,21 @@ int main(int argc,char *argv[])
 	char Rd_buf[128];
 	fp=fopen(argv[1],"w+");
 	//fp=fopen(argv[1],"a+");
-	//
 	if(NULL==fp)
 	{
 		printf("open file %s failure.\n",argv[1]);
 		return -1;
 	}
 	printf("Open file %s success.\n",argv[1]);
+	fputs(Wr_buf,fp);
+	
+	//fgets(Rd_buf,128,fp);
+	//printf("The Rd_buf is %s\n",Rd_buf);	
+	
+	//while(1);			
+	////如果fgets和while都被激活和while都被激活
+	////那么在整个程序暂停后shell可以看到还是有显示
+	////fputs可能使用了刷新内存的函数fflush
 	
 	fclose(fp);
 	return 0;
