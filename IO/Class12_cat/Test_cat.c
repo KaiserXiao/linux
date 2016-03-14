66a5096ad7f6d78a1004316db86b15896273f5b4
KaiserXiao <KaiserFromXiao@gmail.com>
2016-03-14 14:24:11 +0800

    Add class 11

---
+++ IO/Class12_cat/Test_cat.c
@@ -0,0 +1,34 @@
+#include "stdio.h"
+#include "unistd.h"
+#include "fcntl.h"
+#include "string.h"
+int main(int argc,char *argv[])
+{
+	FILE *src_fp;
+	char Read_buf[128];
+	char Read_ret;
+	if(argc<2)
+	{
+		printf("Please input src file\n");
+		return -2;
+	}
+	
+	src_fp=fopen(argv[1],"r");
+	if(src_fp<0)
+	{
+		printf("Open src file %s failure\n",argv[1]);
+		return -1;
+	}
+	printf("Open src file %s success\n",argv[1]);
+
+#if 0
+	while((Read_ret=fgetc(src_fp))&&!feof(src_fp))
+		fputc(Read_ret,stdout);
+	////feof return 1 if end
+#else
+	while(fgets(Read_buf,128,src_fp))
+		fputs(Read_buf,stdout);
+#endif
+	printf("finish\n");
+	fclose(src_fp);	
+}	
