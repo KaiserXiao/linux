006b4f1c60f098cda36f8481be36cdc249836144
KaiserXiao <KaiserFromXiao@gmail.com>
2016-03-14 18:42:55 +0800

    乱码处理

--- IO/Class14_compare/OrderInShell.c
+++ IO/Class14_compare/OrderInShell.c
@@ -1,1 +1,1 @@
-  1 { time ./Test_fgetc a.c fgetc.c ; } >d.c 2>&1  2 { time ./Test_read a.c read.c ; } >>d.c 2>&1//这两句是shell里面用来重定向time 的输出，要掌握起来//说明一下：//文件流fopen与fgetc的调压system call 比较少//故在sys内核运行的时间要少//但每次都只有读取一个字节，用户的时间要比//open与read等系统调用函数的时间要多
+  1 { time ./Test_fgetc a.c fgetc.c ; } >d.c 2>&1  2 { time ./Test_read a.c read.c ; } >>d.c 2>&1////这两句是shell里面用来重定向time 的输出，要掌握起来////说明一下：////文件流fopen与fgetc的调压system call 比较少////故在sys内核运行的时间要少////但每次都只有读取一个字节，用户的时间要比////open与read等系统调用函数的时间要多
