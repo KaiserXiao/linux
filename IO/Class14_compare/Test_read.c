1a04ca1e764e3be4100478311611976f282c50f1
KaiserXiao <KaiserFromXiao@gmail.com>
2016-03-14 18:17:47 +0800

    add class 14

---
+++ IO/Class14_compare/Test_read.c
@@ -0,0 +1,53 @@
+//use open read ,write
+//no wenjian liu 
+//xi tong tiao yong han shu 
+
+
+
+#include "stdio.h"
+#include "unistd.h"
+#include "fcntl.h"
+#include "string.h"
+int main(int argc,char *argv[])
+{
+	int src_fd,des_fd;
+	char Read_buf[128]={0};
+	int  Read_ret;
+	if(argc<3)
+	{
+		printf("Please input src file\n");
+		return -2;
+	}
+	
+	src_fd=open(argv[1],O_RDONLY);
+	if(src_fd<0)
+	{
+		printf("Open src file %s failure\n",argv[1]);
+		return -1;
+	}
+	printf("Open src file %s success\n",argv[1]);
+
+	des_fd=open(argv[2],O_CREAT | O_WRONLY,0777);
+	if(des_fd<0)
+	{
+		printf("Open des file %s failure\n",argv[2]);
+		return -1;
+	}
+	printf("Open des file %s success\n",argv[2]);
+
+	while(1)
+	{
+		Read_ret=read(src_fd,Read_buf,128);
+		write(des_fd,Read_buf,Read_ret);
+		if(Read_ret<128)
+		{
+			printf("read file %s end\n",argv[1]);
+			break;
+		}
+	}
+	
+	printf("\n+++++++++++++++finish++++++++++++++++\n");
+	close(src_fd);
+	close(des_fd);	
+	return 1;
+}	
---
+++ IO/Class14_compare/d.c
@@ -0,0 +1,16 @@
+Open src file a.c success
+Open des file fgetc.c success
+finish
+
+real	0m0.122s
+user	0m0.106s
+sys	0m0.000s
+Open src file a.c success
+Open des file read.c success
+read file a.c end
+
++++++++++++++++finish++++++++++++++++
+
+real	0m0.067s
+user	0m0.009s
+sys	0m0.042s
