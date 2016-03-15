5ab01706114958c58af65d738536e3845e7c24ca
KaiserXiao <KaiserFromXiao@gmail.com>
2016-03-15 17:10:17 +0800

    add class 18 and 19

---
+++ IO/Class18_DirIO/Test_mkdir.c
@@ -0,0 +1,34 @@
+#include "stdio.h"
+#include "sys/types.h"
+#include "dirent.h"
+
+int main()
+{
+	int ret;
+	DIR *dp;
+	struct dirent *dir;	
+	ret =mkdir("./mydir",0x777);  //like open ,umask
+	if (ret<0)
+	{
+		printf("Fail!\n");
+		return -1;
+	}
+	printf("create success! \n");
+
+	dp=opendir("./mydir");
+	if(dp==NULL)
+	{
+		printf("Open fail\n" );
+		return -1;
+	}
+	printf("Open success\n");	
+
+
+	while(dir=readdir(dp))
+		printf("the dp is %ld \nino is %ld\nname is %s\n",telldir(dp),dir->d_ino,dir->d_name);
+	closedir(dp);
+	return 0;
+	
+}
+
+
