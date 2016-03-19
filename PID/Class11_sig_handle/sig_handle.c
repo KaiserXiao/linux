adf300a2bb5881af15d2ab250c21a71ea79b17a7
KaiserXiao <KaiserFromXiao@gmail.com>
2016-03-19 19:51:38 +0800

    第11课多线程信号处理

--- PID/Class11_sig_handle/sig_handle.c
+++ PID/Class11_sig_handle/sig_handle.c
@@ -17,16 +17,31 @@ void *thread_fun1(void *arg)
 {
 	printf("New thread One\n");
 	
+	//sruct sigaction
+	//{
+	//	void(*sa_handler)(int);	
+	//	void(*sa_sigaction)(int ,siginfo *,void *)
+	//	sigset_t sa_mask;
+	//	int sa_flags;
+	//	void(*sa_restorer)(void);
+	//}
+	//LinuxSystemProgramming P334
+		
 	struct sigaction act;
+
+	//首先对结构体act清零
 	memset(&act,0,sizeof(act));
-
+	//对掩码进行处理
 	sigaddset(&act.sa_mask,SIGQUIT);
+	//初始化sa_handler
 	act.sa_handler=sig_handler1;
+	
+	//阻塞或获取信号发生时系统的各个状态？
 	sigaction(SIGQUIT,&act,NULL);
-
 	pthread_sigmask(SIG_BLOCK,&act.sa_mask,NULL);
 	sleep(2);
 	printf("fun One over\n");
+	return ;
 }
 
 
@@ -41,16 +56,18 @@ void *thread_fun2(void *arg)
 	act.sa_handler=sig_handler2;
 	sigaction(SIGQUIT,&act,NULL);
 
-	//pthread_sigmask(SIG_BLOCK,&act.sa_mask,NULL);
+//	pthread_sigmask(SIG_BLOCK,&act.sa_mask,NULL);
+//	pthread_sigmask(SIG_SETMASK,&act.sa_mask,NULL);
 	//对比thread_fun1，屏蔽掉mask，看看效果如何
 	//先test置0，fun1先运行
 	//发现打印出的是Two thread get signal
 	//再test置1，变成了One thread get signal
 	//哪个子线程最后运行，哪个子程序打印出来
-	//这里的课程讲的很不清楚，估计是涉及到很多进程的知识
+	//也就是说首先运行的那个程序被SIGQUIT了
 	sleep(2);
 	
 	printf("fun Two over\n");
+	return ;
 }
 
 int main()
@@ -92,9 +109,10 @@ if(err)
 	}
 #endif
 
-	printf("fist+++++");
+	printf("fist+++++\n");
 	sleep(1);
-	
+
+ 	printf("Begin to kill\n");
 	s=pthread_kill(tid1,SIGQUIT);
 	if(s)
 	{
@@ -105,7 +123,7 @@ if(err)
 	s=pthread_kill(tid2,SIGQUIT);
 	if(s)
 	{
-		printf("Send signal to Two Fail\n");
+	printf("Send signal to Two Fail\n");
 	}
 	
 	pthread_join(tid1,NULL);
